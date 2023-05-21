2d7dcf9e249867ee7395c9c62ce2ff146df5cddf
Hosein Mohamadpor <83702864+younghosein@users.noreply.github.com>
2023-05-21 13:07:16 +0330

    Add files via upload

---
+++ MatrixArrayFunc/MatrixArrayFunc/MA.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatrixArrayFunc
+{
+    class MA
+    {
+        public void BuildMatrix(int[,] a, int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write($"Matrix [{i},{j}] Is : ");
+                    a[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+        }
+        public void PrintMatrix(int[,] a, int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write($"Matrix [{i},{j}] Is : {a[i, j]}\n");
+                }
+            }
+        }
+        public int AvgAboveMainDiameter(int[,] a, int n)
+        {
+            int s = 0, c = 0;
+            for (int i = 1; i < n; i=i+2)
+            {
+                for (int j = i + 1; j < n; j++)
+                {
+                    Console.Write($"Matrix [{i},{j}] Is : {a[i, j]}\n");
+                    s += a[i, j];
+                    c++;
+                }
+            }
+            return s / c;
+        }
+        public void MaxMinAboveMainDiameter(int[,] a, int n)
+        {
+            int max = int.MinValue;
+            int min = int.MaxValue;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (a[i, j] > max)
+                    {
+                        max = a[i, j];
+                    }
+                }
+            }
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (a[i, j] < min)
+                    {
+                        min = a[i, j];
+                    }
+                }
+            }
+            Console.Write("Max : "+max+" , Min : "+min);
+        }
+        public int AvgBottomMainDiameter(int[,] a, int n)
+        {
+            int s = 0, c = 0;
+            for (int i = 1; i < n; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write($"Matrix [{i},{j}] Is : {a[i, j]}\n");
+                    s += a[i, j];
+                    c++;
+                }
+            }
+            return s / c;
+        }
+        public void MaxMinBottomMainDiameter(int[,] a, int n)
+        {
+            int max = int.MinValue;
+            int min = int.MaxValue;
+            for (int i = 1; i < n; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (a[i, j] > max)
+                    {
+                        max = a[i, j];
+                    }
+                }
+            }
+            for (int i = 1; i < n; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (a[i, j] < min)
+                    {
+                        min = a[i, j];
+                    }
+                }
+            }
+            Console.Write("Max : " + max + " , Min : " + min);
+        }
+        public int AvgAboveSubDiameter(int[,] a, int n)
+        {
+            int s = 0, c = 0;
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = 0; j < (n - i) - 1; j++)
+                {
+                    Console.Write($"Matrix [{i},{j}] Is : {a[i, j]}\n");
+                    s += a[i, j];
+                    c++;
+                }
+            }
+            return s / c;
+        }
+        public void MaxMinAboveSubDiameter(int[,] a, int n)
+        {
+            int max = int.MinValue;
+            int min = int.MaxValue;
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = 0; j < (n - i) - 1; j++)
+                {
+                    if (a[i, j] > max)
+                    {
+                        max = a[i, j];
+                    }
+                }
+            }
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = 0; j < (n - i) - 1; j++)
+                {
+                    if (a[i, j] < min)
+                    {
+                        min = a[i, j];
+                    }
+                }
+            }
+            Console.Write("Max : " + max + " , Min : " + min);
+        }
+        public int AvgBottomSubDiameter(int[,] a, int n)
+        {
+            int s = 0, c = 0;
+            for (int i = 1; i < n; i++)
+            {
+                for (int j = (n - i); j < n; j++)
+                {
+                    Console.Write($"Matrix [{i},{j}] Is : {a[i, j]}\n");
+                    s += a[i, j];
+                    c++;
+                }
+
+            }
+            return s / c;
+        }
+        public void MaxMinBottomSubDiameter(int[,] a, int n)
+        {
+            int max = int.MinValue;
+            int min = int.MaxValue;
+            for (int i = 1; i < n; i++)
+            {
+                for (int j = (n - i); j < n; j++)
+                {
+                    if (a[i, j] > max)
+                    {
+                        max = a[i, j];
+                    }
+                }
+            }
+            for (int i = 1; i < n; i++)
+            {
+                for (int j = (n - i); j < n; j++)
+                {
+                    if (a[i, j] < min)
+                    {
+                        min = a[i, j];
+                    }
+                }
+            }
+            Console.Write("Max : " + max + " , Min : " + min);
+        }
+    }
+}
---
+++ MatrixArrayFunc/MatrixArrayFunc/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatrixArrayFunc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            MA ma = new MA();
+            int[,] A;
+            Console.Write("Please Enter The Count Of Matrix : ");
+            int N = int.Parse(Console.ReadLine());
+            A = new int[N, N];
+            ma.BuildMatrix(A, N);
+            Console.WriteLine("\nMatrix Is : ");
+            ma.PrintMatrix(A, N);
+
+            Console.WriteLine("\n\nAbove Main Diameter Element Is : ");
+            int A1 = ma.AvgAboveMainDiameter(A, N);
+            Console.WriteLine("Average Of Elements Is : " + A1);
+            ma.MaxMinAboveMainDiameter(A, N);
+
+            Console.WriteLine("\n\nBottom Main Diameter Element Is : ");
+            int A2 = ma.AvgBottomMainDiameter(A, N);
+            Console.WriteLine("Average Of Elements Is : " + A2);
+            ma.MaxMinBottomMainDiameter(A, N);
+
+            Console.WriteLine("\n\nAbove Sub Diameter Element Is : ");
+            int A3 = ma.AvgAboveSubDiameter(A, N);
+            Console.WriteLine("Average Of Elements Is : " + A3);
+            ma.MaxMinAboveSubDiameter(A, N);
+
+            Console.WriteLine("\n\nBottom Sub Diameter Element Is : ");
+            int A4 = ma.AvgBottomSubDiameter(A, N);
+            Console.WriteLine("Average Of Elements Is : " + A4);
+            ma.MaxMinBottomSubDiameter(A, N);
+
+            Console.ReadKey();
+        }
+    }
+}
